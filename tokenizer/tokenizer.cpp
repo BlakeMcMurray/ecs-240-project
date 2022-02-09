1134778ae1ad751aa162416274907f40fba8beed
Blake McMurray <blake.mcmurray@gmail.com>
2022-02-09 14:48:53 -0800

    added tokenizer primer + input output examples

---
+++ tokenizer/tokenizer.cpp
@@ -0,0 +1,30 @@
+/*
+* File: tokenizer.cpp
+* Author: Blake McMurray
+* Created on 2/9/22
+* Description: This program takes raw text and converts to tokens
+*/
+#include <boost/algorithm/string.hpp>
+#include <iostream>
+using namespace std;
+
+class Tokenizer {       // The class
+    public:             // Access specifier
+        int myNum;        // Attribute (int variable)
+        string myString;  // Attribute (string variable)
+};
+
+class Token {
+    public:
+        string tType;
+        int sPos;
+        string text;
+};
+
+
+int main()
+{   
+
+
+    return(0);
+}
