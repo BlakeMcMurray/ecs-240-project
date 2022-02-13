accfbea250792ffdb0dac527f0ac6b1e0bbc65f2
Blake McMurray <blake.mcmurray@gmail.com>
2022-02-13 15:13:58 -0800

    added comments for readability

--- tokenizer/tokenizer.cpp
+++ tokenizer/tokenizer.cpp
@@ -20,37 +20,47 @@ bool isVariable(string text){
     return(std::regex_match(text, re));
 };
 
+//not implemented
 bool isComparitor(string text){
     bool b = false;
     return(b);
 };
 
+//not implemented
 bool isFor(string text){
     bool b = false;
     return(b);
 };
 
+//not implemented
 bool isWhile(string text){
     bool b = false;
     return(b);
 };
 
+//not implemented
 bool isIf(string text){
     bool b = false;
     return(b);
 };
 
+//not implemented
 bool isComment(string text){
     bool b = false;
     return(b);
 };
 
+//unsure if we need a tokenizer object, but it is here 
+//just in case
 class Tokenizer {      
     public:            
         int myNum;        
         string myString;  
 };
 
+//token object that holds the type of token,
+//the position in the original text and the 
+//raw text that the token consists of
 class Token {
     public:
         string tType;
@@ -67,33 +77,33 @@ int main()
 {   
 
     string line;
+
+    //input file that the child types into a code block
     ifstream test(inFile2);
+
+    //vector to contain each raw line of text
     vector<string> rawLines;
+
+    //vector of tokens to be returned or printed to a file
     vector<Token> tokens;
 
+    //generates the lines
     while (getline (test, line)) {
         rawLines.push_back(line);
     }
 
-
-
     vector<string> splitted;
     for (int i = 0; i < rawLines.size(); i++) {
+
+        //splits a string into a vector of strings by space delimiter
         boost::split(splitted, rawLines[i], boost:: is_any_of(" "));
         
         for(int j = 0; j < splitted.size(); j++){
-            TokenNames tNames;
-
+            //TODO
         }
 
     }
 
-
-
-
-
-    
-    // Close the file
     test.close();
     return(0);
 }
